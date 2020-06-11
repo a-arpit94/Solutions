291bc1d1cbc8f96e8cae0c677c9891fda5dad1fa
arpit agarwal <a.arpit94@gmail.com>
2020-06-12 01:00:10 +0530

    added BFS and DFS algos in tree

---
+++ LeetCode.Solutions/Easy/LeetTrees/BFS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Common.Setup;
+using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Easy.LeetTrees
+{
+    public class BFS : IOperation
+    {
+        public void Execute(DataTypes dataTypes)
+        {
+            dataTypes.InputTree = new TreeNode(6);
+            dataTypes.InputTree.InsertNode(new TreeNode(3));
+            dataTypes.InputTree.InsertNode(new TreeNode(8));
+            dataTypes.InputTree.InsertNode(new TreeNode(2));
+            dataTypes.InputTree.InsertNode(new TreeNode(13));
+            dataTypes.InputTree.InsertNode(new TreeNode(10));
+            dataTypes.InputTree.InsertNode(new TreeNode(4));
+            dataTypes.InputTree.InsertNode(new TreeNode(14));
+            foreach (var i in Implementation(dataTypes.InputTree))
+                Console.WriteLine(i);
+        }
+
+        private IEnumerable<int> Implementation(TreeNode root)
+        {
+            if (root == null) yield return int.MinValue;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                root = queue.Dequeue();
+                if(root.Left != null) queue.Enqueue(root.Left);
+                if(root.Right != null) queue.Enqueue(root.Right);
+                yield return root.Val;
+            }
+
+        }
+
+    }
+}
---
+++ LeetCode.Solutions/Easy/LeetTrees/DFS.cs
@@ -0,0 +1,38 @@
+﻿using Common.Setup;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Easy.LeetTrees
+{
+    public class DFS : IOperation
+    {
+        public void Execute(DataTypes dataTypes)
+        {
+            dataTypes.InputTree = new TreeNode(6);
+            dataTypes.InputTree.InsertNode(new TreeNode(3));
+            dataTypes.InputTree.InsertNode(new TreeNode(8));
+            dataTypes.InputTree.InsertNode(new TreeNode(2));
+            dataTypes.InputTree.InsertNode(new TreeNode(13));
+            dataTypes.InputTree.InsertNode(new TreeNode(10));
+            dataTypes.InputTree.InsertNode(new TreeNode(4));
+            dataTypes.InputTree.InsertNode(new TreeNode(14));
+            foreach (var i in Implementation(dataTypes.InputTree))
+                Console.WriteLine(i);
+        }
+
+        private IEnumerable<int> Implementation(TreeNode root)
+        {
+            if (root == null) yield return int.MinValue;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                root = stack.Pop();
+                if(root.Left != null) stack.Push(root.Left);
+                if(root.Right != null) stack.Push(root.Right);
+                yield return root.Val;
+            }
+
+        }
+    }
+}
